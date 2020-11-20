0a456180305ae7e3a8fa30f03c1558598ea7e311
Alibay74 <aitov.74@list.ru>
2020-11-20 17:12:08 -0500

    added AlibayPush for IT Guys

---
+++ src/PushPacket/PushClass1.java
@@ -0,0 +1,19 @@
+package PushPacket;
+
+public class PushClass1 {
+
+public static void main(String[] args) {
+boolean a="Alibay" != null;
+String b="Aitov";
+boolean b1 = true;
+
+if (a = true ); 
+System.out.println("This is " +a);
+//else ( ){	
+System.out.println("This is " +b);
+
+}
+
+	}
+
+
